4b046d74f1fcfb8e116e7c3a90156b4d7f61cfe1
maxvodoo <ahmed.hararaa@gmail.com>
2019-03-19 03:24:09 +0200

    Update RCTBluetoothSerialPackage.java

--- android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
+++ android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
@@ -12,19 +12,16 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RCTBluetoothSerialPackage implements ReactPackage {
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RCTBluetoothSerialModule(reactContext));
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
